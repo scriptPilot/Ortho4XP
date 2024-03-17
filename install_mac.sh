3b2586bba5f8c45b589196a38574a23df9699657
scriptPilot <dennis@scheffel.de>
2024-03-17 01:31:02 +0100

    install Python only if not yet installed

--- install_mac.sh
+++ install_mac.sh
@@ -1,8 +1,18 @@
+# Install Python if not found
+if command -v python3 &>/dev/null; then
+    echo "python3 already available"
+else
+    brew install python
+fi
+
 # Install dependencies
-brew install python gdal spatialindex p7zip proj python-tk
+brew install gdal spatialindex p7zip proj python-tk
 
 # Install pyproj
 pip3 install pyproj
 
 # Install other dependencies
-pip3 install numpy shapely rtree pillow requests scikit-fmm
+pip3 install numpy shapely rtree pillow requests
+
+# Install new dependency scikit-fmm
+pip3 install scikit-fmm
